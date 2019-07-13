d9ec0003fb4228585d6339df2949afd71d64d589
Sam Walker <sewalker191@gmail.com>
2019-07-12 21:26:06 -0700

    Setup express server and connect to mongodb

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const mongoose = require('mongoose');
 const app = express();
+
+const db = require('./config/keys').mongoURI;
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Sup Dawg"));
 
