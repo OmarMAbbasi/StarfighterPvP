b47921c2959ef685086800389f641a996101089c
Omar Abbasi <omarmabbasi@gmail.com>
2019-07-13 13:23:44 -0700

    Add env variables to app.js for heroku deploy

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require("express");
-const mongoose = require('mongoose');
-const db = require('./config/keys').mongoURI;
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
 const app = express();
+const path = require("path");
 
 mongoose
-    .connect(db, { useNewUrlParser: true })
-    .then(() => console.log("Connected to MongoDB successfully"))
-    .catch(err => console.log(err));
+	.connect(db, { useNewUrlParser: true })
+	.then(() => console.log("Connected to MongoDB successfully"))
+	.catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Sup Dawg"));
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("frontend/build"));
+	app.get("/", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+	});
+}
+
