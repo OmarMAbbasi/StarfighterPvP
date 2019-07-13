5423f0f81198c10582211808ce7592ebf58a7772
Sam Walker <sewalker191@gmail.com>
2019-07-13 13:43:20 -0700

    Add score model, add bodyparser to app

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ const express = require("express");
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
 const app = express();
+const bodyParser = require('body-parser');
 
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => res.send("Sup Dawg"));
 
