e3729f69ae6686488795364af1e916d0d1d8e1c4
Sam Walker <sewalker191@gmail.com>
2019-07-12 22:13:29 -0700

    Add debug script to package.json

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
 const app = express();
 
-const db = require('./config/keys').mongoURI;
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
