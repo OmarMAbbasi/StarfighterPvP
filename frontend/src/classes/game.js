265953b7d56d85d8845331f69b15dd2a18a70ffb
Sam Walker <sewalker191@gmail.com>
2019-07-13 16:32:56 -0700

    Add game

---
+++ frontend/src/classes/game.js
@@ -0,0 +1,28 @@
+class Game {
+    constructor(numRounds, roundLength) {
+        this.width = 1600;
+        this.height = 900;
+        this.players = [];
+        this.hazards = [];
+        this.bullets = [];
+        this.rounds = numRounds;
+        this.timer = roundLength;
+    }
+
+    startGame() {
+
+    }
+
+    playRound() {
+
+    }
+
+    selectPowerups() {
+
+    }
+    gameOver() {
+
+    }
+}
+
+module.exports = Game;
