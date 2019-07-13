416af6633732534c3207bbf0e0114e917ab49ee7
Sam Walker <sewalker191@gmail.com>
2019-07-12 20:08:01 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Sup Dawg"));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
