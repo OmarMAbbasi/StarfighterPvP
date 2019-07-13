67eee96be6b3866ba75c4bad61c214144fc7e30e
mwdeshaw <mwdeshaw@gmail.com>
2019-07-13 15:19:15 -0700

    Create scores post and get routes, as well as api utils

--- frontend/src/utils/high_score_routes.js
+++ frontend/src/utils/high_score_routes.js
@@ -1,13 +1,9 @@
-// import axios from 'axios';
+import axios from 'axios';
 
-// export const getTweets = () => {
-//     return axios.get('/api/tweets')
-// };
+export const getScores = () => {
+    return axios.get('/api/scores')
+};
 
-// export const getUserTweets = id => {
-//     return axios.get(`/api/tweets/user/${id}`)
-// };
-
-// export const writeTweet = data => {
-//     return axios.post('/api/tweets/', data)
-// }
+export const createHighScore = data => {
+    return axios.post('/api/scores', data)
+}
