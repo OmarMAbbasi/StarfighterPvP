46452442012f0b92a85803700de6b7517a39f6c5
mwdeshaw <mwdeshaw@gmail.com>
2019-07-13 14:36:59 -0700

    Move routes to new file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,6 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const bodyParser = require('body-parser');
-App.use(bodyParser.urlencoded({ extended: false }));
-App.use(bodyParser.json());
-
-//adding the highscores route
-const highscores = require("./routes/api/highscores");
-App.use('api/highscores', highscores)
-
-
 function App() {
   return (
     <div className="App">
