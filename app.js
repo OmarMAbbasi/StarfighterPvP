e598524c5cfd26c30df53faedd19fbbdc85f745f
Omar Abbasi <omarmabbasi@gmail.com>
2019-07-14 14:02:52 -0700

    It was server all along

--- app.js
+++ app.js
@@ -1,49 +1,47 @@
 const express = require("express");
 
 //! Off while dev websockets
-// const mongoose = require("mongoose");
-// const db = require("./config/keys").mongoURI;
-// const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+const app = express();
+const http = require("http");
+const serv = http.Server(app);
 
-const app = require("http").createServer();
 const path = require("path");
 
 const bodyParser = require("body-parser");
 
-// const player = require("./models/player");
+const player = require("./models/player");
 
-//websockets
-// var serv = require("http").createServer();
-var io = (module.exports.io = require("socket.io")(app));
+const io = (module.exports.io = require("socket.io")(serv));
 const SocketManager = require("./frontend/src/SocketManager.js");
 
 io.on("connection", SocketManager);
 //! Off while dev websockets
-// mongoose
-// 	.connect(db, { useNewUrlParser: true })
-// 	.then(() => console.log("Connected to MongoDB successfully"))
-// 	.catch(err => console.log(err));
+mongoose
+	.connect(db, { useNewUrlParser: true })
+	.then(() => console.log("Connected to MongoDB successfully"))
+	.catch(err => console.log(err));
 
-// app.use(
-// 	bodyParser.urlencoded({
-// 		extended: false
-// 	})
-// );
+app.use(
+	bodyParser.urlencoded({
+		extended: false
+	})
+);
 
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 
-// app.get("/", (req, res) => {
-// 	res.send("Sup Dawg");
-// });
+app.get("/", (req, res) => {
+	res.send("Sup Dawg");
+});
 
-// const players = require("./routes/api/players");
-// app.use("/api/players", players);
+const players = require("./routes/api/players");
+app.use("/api/players", players);
 //! Off
-
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+serv.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static("frontend/build"));
