98b6d731d2b7cb308d0ce28de1be769355b88fd1
mwdeshaw <mwdeshaw@gmail.com>
2019-07-13 18:42:46 -0700

    put store on window

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -8,7 +8,8 @@ import configureStore from './store/store';
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById("root")
     const store = configureStore();
-
+    
+    window.store = store;
 
     ReactDOM.render(<Root store={store} />, root);
 })
