79564ca8773562cd7df815770e19fd444d466804
Omar Abbasi <omarmabbasi@gmail.com>
2019-07-15 14:32:43 -0700

    Fix merge error

--- frontend/src/components/game.jsx
+++ frontend/src/components/game.jsx
@@ -8,16 +8,24 @@ class Canvas extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			socket: null
+			socket: null,
+			hazards: this.props.hazards,
+			input: {
+				w: false,
+				s: false,
+				a: false,
+				d: false
+			}
 		};
 		// this.state = this.props;
 		this.openSocket = this.openSocket.bind(this);
+		this._handleKey = this._handleKey.bind(this);
 		this.canvasRef = React.createRef();
 	}
 
 	openSocket = () => {
-		const socket = io(socketURL);
-		this.setState({ socket: { socket } });
+		let socket = io(socketURL);
+		this.setState({ socket });
 
 		//!Socket Tests
 		socket.on("connect", () => {
@@ -27,8 +35,29 @@ class Canvas extends React.Component {
 		socket.emit("c2s", {
 			event: "Client Talks to Server"
 		});
-
 		socket.on("s2c", data => console.log(data.event));
+
+		socket.on("newPosition", data => {
+			let hazards = this.state.hazards;
+			// this.setState({ hazards: hazards });
+			const canvas = this.canvasRef.current;
+			const ctx = canvas.getContext("2d");
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+			// this.state.hazards
+			let newHazArr = [];
+			if (this.props !== {} && this.state.hazards !== {}) {
+				// this.state.players.forEach((player) => player.draw(ctx))
+				for (let i = 0; i < data.length; i++) {
+					let newHazard = hazards[0];
+					newHazard = Object.assign(newHazard, data[i]);
+					newHazArr.push(newHazard);
+					newHazard.draw(ctx);
+				}
+				this.setState({ hazards: newHazArr });
+
+				// this.state.bullets.forEach((bullet) => bullet.draw(ctx))
+			}
+		});
 	};
 
 	_handleKey(event, down) {
@@ -38,7 +67,7 @@ class Canvas extends React.Component {
 
 		switch (event.keyCode) {
 			case 87:
-				if (input.w !== down) {
+				if (input.w != down) {
 					input.w = down;
 					socket.emit("playerInput", input);
 					console.log(input);
@@ -46,7 +75,7 @@ class Canvas extends React.Component {
 
 				break;
 			case 83:
-				if (input.s !== down) {
+				if (input.s != down) {
 					input.s = down;
 					socket.emit("playerInput", input);
 					console.log(input);
@@ -54,14 +83,14 @@ class Canvas extends React.Component {
 
 				break;
 			case 65:
-				if (input.a !== down) {
+				if (input.a != down) {
 					input.a = down;
 					socket.emit("playerInput", input);
 					console.log(input);
 				}
 				break;
 			case 68:
-				if (input.d !== down) {
+				if (input.d != down) {
 					input.d = down;
 					socket.emit("playerInput", input);
 					console.log(input);
@@ -87,11 +116,17 @@ class Canvas extends React.Component {
 		const canvas = this.canvasRef.current;
 		const ctx = canvas.getContext("2d");
 		ctx.fillRect(0, 0, canvas.width, canvas.height);
-		if (this.props !== {}) {
+		if (this.state !== {}) {
 			// this.state.players.forEach((player) => player.draw(ctx))
-			this.props.hazards.forEach(hazard => hazard.draw(ctx));
+			this.state.hazards.forEach(hazard => hazard.draw(ctx));
 			// this.state.bullets.forEach((bullet) => bullet.draw(ctx))
 		}
+		document.addEventListener("keydown", event => {
+			this._handleKey(event, true);
+		});
+		document.addEventListener("keyup", event => {
+			this._handleKey(event, false);
+		});
 	}
 
 	render() {
