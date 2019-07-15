300b149928ef9b3a6bb51cf28f51ca0260b298e8
Omar Abbasi <omarmabbasi@gmail.com>
2019-07-15 15:37:14 -0700

    formattign

--- src/classes/bullet.js
+++ src/classes/bullet.js
@@ -1,52 +1,51 @@
-const MovingObject = require('./movingObject');
-const redBlast = require('../style/images/redblast.png');
-const Player = require('./player');
+const MovingObject = require("./movingObject");
+const redBlast = require("../style/images/redblast.png");
+const Player = require("./player");
 // const Hazard = require('./hazard');
 
 class Bullet extends MovingObject {
-    constructor(pos, vel, size, playerId, damage) {
-        super(pos, vel, size);
-        this.playerId = playerId;
-        this.damage = damage;
-    }
+	constructor(pos, vel, size, playerId, damage) {
+		super(pos, vel, size);
+		this.playerId = playerId;
+		this.damage = damage;
+	}
 
-    rewardPoints(players, obj) {
-        if (this.isCollidedWith(obj) && obj.health <= 0) {
-            players.forEach(player => {
-                if (player.id === this.playerId) {
-                    player.addScore(obj.points);
-                }
-            })
-        }
-    }
+	rewardPoints(players, obj) {
+		if (this.isCollidedWith(obj) && obj.health <= 0) {
+			players.forEach(player => {
+				if (player.id === this.playerId) {
+					player.addScore(obj.points);
+				}
+			});
+		}
+	}
 
-    draw(ctx) {
-        // to see hit circle
-        // ctx.fillStyle = "#00FF00";
-        // ctx.beginPath();
-        // ctx.arc(this.pos.x, this.pos.y, this.radius, 0, 2 * Math.PI, true);
-        // ctx.fill();
-        // ctx.closePath();
-        //
-        
-        let img = new Image();
-        // let rotateDir;
-        // if (this.dir.x === 0) {
-        //     rotateDir = (this.dir.y * (-Math.PI / 2))
-        // } else {
-        //     rotateDir = Math.atan(this.dir.y / this.dir.x);
-        // }
-        img.onload = () => {
-            // ctx.save();
-            // ctx.translate(this.pos.x, this.pos.y);
-            // ctx.rotate(rotateDir);
-            // ctx.translate(-this.pos.x, -this.pos.y);
-            ctx.drawImage(img, this.pos.x - 6, this.pos.y - 5, 12, 15);
-            // ctx.restore();
+	draw(ctx) {
+		// to see hit circle
+		// ctx.fillStyle = "#00FF00";
+		// ctx.beginPath();
+		// ctx.arc(this.pos.x, this.pos.y, this.radius, 0, 2 * Math.PI, true);
+		// ctx.fill();
+		// ctx.closePath();
+		//
 
-        };
-        img.src = redBlast;
-    }
+		let img = new Image();
+		// let rotateDir;
+		// if (this.dir.x === 0) {
+		//     rotateDir = (this.dir.y * (-Math.PI / 2))
+		// } else {
+		//     rotateDir = Math.atan(this.dir.y / this.dir.x);
+		// }
+		img.onload = () => {
+			// ctx.save();
+			// ctx.translate(this.pos.x, this.pos.y);
+			// ctx.rotate(rotateDir);
+			// ctx.translate(-this.pos.x, -this.pos.y);
+			ctx.drawImage(img, this.pos.x - 6, this.pos.y - 5, 12, 15);
+			// ctx.restore();
+		};
+		img.src = redBlast;
+	}
 }
 
 module.exports = Bullet;
