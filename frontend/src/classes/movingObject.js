58c496060d4520df85eb8d9992bcdaae1fa309c8
Omar Abbasi <omarmabbasi@gmail.com>
2019-07-14 21:02:15 -0700

    Change pos and vol in movobj to pojo

--- frontend/src/classes/movingObject.js
+++ frontend/src/classes/movingObject.js
@@ -1,34 +1,38 @@
-const Util = require('../utils/game_util');
+const Util = require("../utils/game_util");
 
 class MovingObject {
-    constructor(pos, vel, radius) {
-        this.pos = pos;
-        this.vel = vel;
-        this.radius = radius;
-    }
+	constructor(pos, vel, radius) {
+		//* {x: 0, y:0}
+		this.pos = pos;
+		//*{x: 0, y: 0}
+		this.vel = vel;
+		this.radius = radius;
+	}
 
-    checkCollision(other) {
-        const ctrPointDist = Util.dist(this.pos, other.pos);
-        return ctrPointDist < (this.radius + other.radius);
-    }
+	checkCollision(other) {
+		const ctrPointDist = Util.dist(this.pos, other.pos);
+		return ctrPointDist < this.radius + other.radius;
+	}
 
-    applyPowerups(powerups) {
+	applyPowerups(powerups) {}
 
-    }
+	move() {
+		this.pos = {
+			x: this.pos.x + this.vel.x,
+			y: this.pos.y + this.vel.y
+		};
+		// this.pos = [this.pos[0] + this.vel[0], this.pos[1] + this.vel[1]];
+	}
 
-    move() {
-        this.pos = [(this.pos[0] + this.vel[0]), (this.pos[1] + this.vel[1])]
-    }
+	draw(ctx) {
+		ctx.fillStyle = "#00FF00";
 
-    draw(ctx) {
-        ctx.fillStyle = '#00FF00';
+		ctx.beginPath();
 
-        ctx.beginPath();
-        ctx.arc(
-            this.pos[0], this.pos[1], this.radius, 0, 2 * Math.PI, true
-        );
-        ctx.fill();
-    }
+		ctx.arc(this.pox.x, this.pos.y, this.radius, 0, 2 * Math.PI, true);
+		// ctx.arc(this.pos[0], this.pos[1], this.radius, 0, 2 * Math.PI, true);
+		ctx.fill();
+	}
 }
 
 module.exports = MovingObject;
