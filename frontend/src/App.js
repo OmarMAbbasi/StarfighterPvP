21b84c89eac5566f345d38bb145bedf05a9f2a55
mwdeshaw <mwdeshaw@gmail.com>
2019-07-14 20:24:54 -0700

    Create images folder in assets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 // import logo from "./logo.svg";
 // import './App.css'
 import './stylesheets/player_form.css';
-import SocketTest from "./components/sockettest";
 import GameContainer from "./components/game_container";
 import PlayerContainer from './components/player_container';
 import { Switch, Route } from 'react-router-dom';
@@ -10,7 +9,6 @@ import { Switch, Route } from 'react-router-dom';
 function App() {
 	return (
 		<div className="App">
-			<SocketTest />
 			{/* <header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 				<p>
