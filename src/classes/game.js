6f488b54c7f1a0286e3b47225840823a689b44f4
Michael Du <michaeldu.work@gmail.com>
2019-07-16 15:10:48 -0700

    bullet logic

--- src/classes/game.js
+++ src/classes/game.js
@@ -7,98 +7,99 @@ const HAZARD_COUNT = 20;
 const NUM_ROUNDS = 5;
 const ROUND_LENGTH = 30;
 const START_LOCS = [
-	{ pos: { x: 200, y: 200 }, dir: { x: 1, y: 0 } },
-	{ pos: { x: 1400, y: 700 }, dir: { x: -1, y: 0 } },
-	{ pos: { x: 200, y: 700 }, dir: { x: 1, y: 0 } },
-	{ pos: { x: 1400, y: 200 }, dir: { x: -1, y: 0 } }
+    { pos: { x: 200, y: 200 }, dir: { x: 1, y: 0 } },
+    { pos: { x: 1400, y: 700 }, dir: { x: -1, y: 0 } },
+    { pos: { x: 200, y: 700 }, dir: { x: 1, y: 0 } },
+    { pos: { x: 1400, y: 200 }, dir: { x: -1, y: 0 } }
 ];
 
 class Game {
-	constructor(gameId, hostId, numRounds = NUM_ROUNDS, roundLength = ROUND_LENGTH) {
-		// set game parameters
-		this.rounds = numRounds;
-		this.roundLength = roundLength;
+    constructor(gameId, hostId, numRounds = NUM_ROUNDS, roundLength = ROUND_LENGTH) {
+        // set game parameters
+        this.rounds = numRounds;
+        this.roundLength = roundLength;
 
-		// set game host
-		this.hostId = hostId;
+        // set game host
+        this.hostId = hostId;
 
-		// create players array with host POJO
-		// this.players = [new Player(START_LOCS[0].pos, hostId, START_LOCS[0].dir)];
-		this.players = {};
+        // create players array with host POJO
+        // this.players = [new Player(START_LOCS[0].pos, hostId, START_LOCS[0].dir)];
+        this.players = {};
 
-		// create hazards array
-		this.hazards = [];
+        // create hazards array
+        this.hazards = [];
 
-		// create empty bullets array
-		this.bullets = [];
+        // create empty bullets array
+        this.bullets = [];
 
         this.playerSockets = {};
-        
+
         this.timer = 0;
-	}
+    }
 
-	async startGame() {
-		console.log("Starting game");
-		while (this.rounds > 0) {
-			await this.playRound();
-			this.rounds--;
-		}
-		this.gameOver();
-	}
+    async startGame() {
+        console.log("Starting game");
+        while (this.rounds > 0) {
+            await this.playRound();
+            this.rounds--;
+        }
+        this.gameOver();
+    }
 
-	async playRound() {
-		console.log(`Round ${this.rounds}`);
-		const sleep = ms => new Promise(res => setTimeout(res, ms));
+    async playRound() {
+        console.log(`Round ${this.rounds}`);
+        const sleep = ms => new Promise(res => setTimeout(res, ms));
 
-		this.initRound();
+        this.initRound();
 
-		this.lastUpdate = Date.now();
-		while (this.timer > 0) {
-			this.update();
-			this.lastUpdate = Date.now();
-			await sleep(1000 / FPS);
-		}
-	}
+        this.lastUpdate = Date.now();
+        while (this.timer > 0) {
+            this.update();
+            this.lastUpdate = Date.now();
+            await sleep(1000 / FPS);
+        }
+    }
 
-	update() {
-		// calculate time since last update
-		const deltaTime = (Date.now() - this.lastUpdate) / 1000;
-		// decrease time remaining in round
-		this.timer -= deltaTime;
+    update() {
+        // calculate time since last update
+        const deltaTime = (Date.now() - this.lastUpdate) / 1000;
+        // decrease time remaining in round
+        this.timer -= deltaTime;
 
-		let allObjects = this.allObjects();
+        let allObjects = this.allObjects();
 
-		Object.values(this.players).forEach(player => {
-			if (player.shooting) {
-                let bullet = player.shoot(deltaTime);
-				if (bullet) console.log(player.shoot());
-			}
-		});
-		// move all objects
-		allObjects.forEach(obj => obj.move(deltaTime));
+        Object.values(this.players).forEach(player => {
+            let bullet = player.shoot(deltaTime);
+            if (bullet) {
+                console.log(bullet);
+                this.bullets.push(bullet);
+            }
+        });
+        // move all objects
+        allObjects.forEach(obj => obj.move(deltaTime));
 
-		// check collisions
-		Object.values(this.players).forEach(player => {
-			this.hazards.concat(this.bullets).forEach(obj2 => {
-				player.collideWith(obj2)
-			})
-		})
+        // check collisions
+        Object.values(this.players).forEach(player => {
+            this.hazards.concat(this.bullets).forEach(obj2 => {
+                player.collideWith(obj2)
+            })
+        })
 
-		// update clients with new positions
-		Object.values(this.playerSockets).forEach(socket => {
-			// emit game state to client
-			socket.emit("newPosition", { 
+        // update clients with new positions
+        Object.values(this.playerSockets).forEach(socket => {
+            // emit game state to client
+            socket.emit("newPosition", {
                 players: Object.values(this.players),
                 hazards: this.hazards,
                 bullets: this.bullets,
                 score: this.players[socket.id].score,
                 timer: this.timer,
                 rounds: this.rounds
-             });
-		});
-		// console.log(this.bullets);
+            });
+        });
+        // console.log(this.bullets);
     }
-    
+
     removeObject(obj) {
         if (obj instanceof Bullet) {
             this.bullets.splice(this.bullets.indexOf(obj), 1);
@@ -107,47 +108,47 @@ class Game {
         }
     }
 
-	selectPowerups() {}
+    selectPowerups() {}
 
-	gameOver() {}
+    gameOver() {}
 
-	addPlayer(playerId, socket, playerTag, gameId) {
-		if (this.players.length === 4) {
-			return null;
-		};
-		
-		let playerParams = START_LOCS[Object.keys(this.players).length];
-		let player = new Player(playerParams.pos, playerId, playerParams.dir);
+    addPlayer(playerId, socket, playerTag, gameId) {
+        if (this.players.length === 4) {
+            return null;
+        };
+
+        let playerParams = START_LOCS[Object.keys(this.players).length];
+        let player = new Player(playerParams.pos, playerId, playerParams.dir);
         player.playerTag = playerTag;
         player.gameId = gameId;
-		this.players[playerId] = player;
-		this.playerSockets[playerId] = socket;
-		return player;
-	}
+        this.players[playerId] = player;
+        this.playerSockets[playerId] = socket;
+        return player;
+    }
 
-	removePlayer(playerId) {
-		delete this.players[playerId];
-		delete this.playerSockets[playerId];
-	}
+    removePlayer(playerId) {
+        delete this.players[playerId];
+        delete this.playerSockets[playerId];
+    }
 
-	populateHazards() {
-		this.hazards = [];
-		for (let i = 0; i < HAZARD_COUNT; i++) {
-			const hazard = new Hazard();
-			this.hazards.push(hazard);
-		}
-		console.log(this.hazards.length);
-	}
+    populateHazards() {
+        this.hazards = [];
+        for (let i = 0; i < HAZARD_COUNT; i++) {
+            const hazard = new Hazard();
+            this.hazards.push(hazard);
+        }
+        console.log(this.hazards.length);
+    }
 
-	initRound() {
-		this.populateHazards();
-		this.bullets = [];
-		this.timer = this.roundLength;
-	}
+    initRound() {
+        this.populateHazards();
+        this.bullets = [];
+        this.timer = this.roundLength;
+    }
 
-	allObjects() {
-		return [].concat(Object.values(this.players), this.hazards, this.bullets);
-	}
+    allObjects() {
+        return [].concat(Object.values(this.players), this.hazards, this.bullets);
+    }
 }
 
 module.exports = Game;
--- src/classes/player.js
+++ src/classes/player.js
@@ -9,113 +9,113 @@ const BULLET_SPEED = 300;
 const FIRE_RATE = 2;
 
 class Player extends MovingObject {
-	constructor(pos, id, dir) {
-		super(pos, { x: 0, y: 0 }, PLAYER_RADIUS);
-		this.id = id;
-		this.health = 100;
-		this.totalScore = 0;
-		this.score = 0;
-		this.points = 1000;
-		this.inputs = {};
-		this.dir = dir;
-		this.speed = 0;
+    constructor(pos, id, dir) {
+        super(pos, { x: 0, y: 0 }, PLAYER_RADIUS);
+        this.id = id;
+        this.health = 100;
+        this.totalScore = 0;
+        this.score = 0;
+        this.points = 1000;
+        this.inputs = {};
+        this.dir = dir;
+        this.speed = 0;
         this.shooting = false;
         this.lastShotDelta = 1000 / FIRE_RATE;
-	}
+    }
 
-	collideWith(obj) {
-		if (this.isCollidedWith(obj)) {
-			console.log("im hit");
-			if (obj instanceof Player || obj instanceof Hazard) {
-				this.takeDamage(100);
-				obj.takeDamage(100);
-			} else if (obj instanceof Bullet) {
-				this.takeDamage(obj.damage);
-			}
-		}
-	}
+    collideWith(obj) {
+        if (this.isCollidedWith(obj)) {
+            console.log("im hit");
+            if (obj instanceof Player || obj instanceof Hazard) {
+                this.takeDamage(100);
+                obj.takeDamage(100);
+            } else if (obj instanceof Bullet && obj.playerId !== this.id) {
+                this.takeDamage(obj.damage);
+            }
+        }
+    }
 
-	addScore(points) {
-		this.score += points;
-		this.totalScore += points;
-	}
+    addScore(points) {
+        this.score += points;
+        this.totalScore += points;
+    }
 
-	takeDamage(damage) {
-		this.health -= damage;
-		if (this.health <= 0) {
-			this.respawn();
-		}
-	}
+    takeDamage(damage) {
+        this.health -= damage;
+        if (this.health <= 0) {
+            this.respawn();
+        }
+    }
 
-	respawn() {
-		this.health = 100;
-		this.pos = { x: Math.random() * 1200 + 200, y: Math.random() * 500 + 200 };
-	}
+    respawn() {
+        this.health = 100;
+        this.pos = { x: Math.random() * 1200 + 200, y: Math.random() * 500 + 200 };
+    }
 
-	shoot(deltaTime) {
+    shoot(deltaTime) {
         if (this.lastShotDelta < 1000 / FIRE_RATE || !this.inputs.space) {
             this.lastShotDelta += deltaTime;
             return;
         }
 
-        this.lastShotDelta = 0;
-		let vel = { x: this.vel.x * 3, y: this.vel.y * 3 };
-		let bullet = new Bullet(
-			this.pos,
-			[this.dir.x * BULLET_SPEED, this.dir.y * BULLET_SPEED],
-			5,
-			this.id,
-			10
+        let vel = { x: this.vel.x * 3, y: this.vel.y * 3 };
+        let bullet = new Bullet(
+            this.pos, [this.dir.x * BULLET_SPEED, this.dir.y * BULLET_SPEED],
+            5,
+            this.id,
+            10
         );
-		return bullet; //console.log(input);
-	}
 
-	setInputs(inputs) {
-		console.log(inputs);
-		this.inputs = inputs;
-		if (inputs.w) {
-			this.speed = PLAYER_SPEED;
-		} else {
-			this.speed = 0;
-		}
-		if (inputs.space) {
-			this.shooting = true;
-		} else {
-			this.shooting = false;
-		}
-	}
+        this.lastShotDelta = 0;
+        return bullet; //console.log(input);
+    }
 
-	move(deltaTime) {
-		// rotate player
+    setInputs(inputs) {
+        console.log(inputs);
+        this.inputs = inputs;
+        if (inputs.w) {
+            this.speed = PLAYER_SPEED;
+        } else {
+            this.speed = 0;
+        }
+        if (inputs.space) {
+            this.shooting = true;
+        } else {
+            this.shooting = false;
+        }
+    }
+
+    move(deltaTime) {
+        // rotate player
         this.rotate(deltaTime);
-		this.pos.x += this.dir.x * this.speed * deltaTime;
-		this.pos.y += this.dir.y * this.speed * deltaTime;
-		// check if offscreen
-		this.screenWrap();
-	}
+        this.pos.x += this.dir.x * this.speed * deltaTime;
+        this.pos.y += this.dir.y * this.speed * deltaTime;
+        // check if offscreen
+        this.screenWrap();
+    }
 
-	rotate(deltaTime) {
-		let dir = 0;
+    rotate(deltaTime) {
+        let dir = 0;
 
-		// check inputs for rotate direction
-		if (this.inputs.a && this.inputs.d) {
-			dir = 0;
-		} else if (this.inputs.a) {
-			dir = -1;
-		} else if (this.inputs.d) {
-			dir = 1;
-		}
+        // check inputs for rotate direction
+        if (this.inputs.a && this.inputs.d) {
+            dir = 0;
+        } else if (this.inputs.a) {
+            dir = -1;
+        } else if (this.inputs.d) {
+            dir = 1;
+        }
 
-		this.rotateVector([this.dir.x, this.dir.y], dir * ROTATE_SPEED * deltaTime);
-	}
+        this.rotateVector([this.dir.x, this.dir.y], dir * ROTATE_SPEED * deltaTime);
+    }
 
-	rotateVector(vec, ang) {
-		ang = -ang * (Math.PI / 180);
-		var cos = Math.cos(ang);
-		var sin = Math.sin(ang);
-		this.dir.x = Math.round(10000 * (vec[0] * cos - vec[1] * sin)) / 10000;
-		this.dir.y = Math.round(10000 * (vec[0] * sin + vec[1] * cos)) / 10000;
-	}
+    rotateVector(vec, ang) {
+        ang = -ang * (Math.PI / 180);
+        var cos = Math.cos(ang);
+        var sin = Math.sin(ang);
+        this.dir.x = Math.round(10000 * (vec[0] * cos - vec[1] * sin)) / 10000;
+        this.dir.y = Math.round(10000 * (vec[0] * sin + vec[1] * cos)) / 10000;
+    }
 }
 
 module.exports = Player;
