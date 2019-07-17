08e1208dc54ff83f167e715f5bf5278bcbf0bc67
mwdeshaw <mwdeshaw@gmail.com>
2019-07-16 19:47:46 -0700

    Reformat the health bar

--- frontend/src/components/player_list_item.jsx
+++ frontend/src/components/player_list_item.jsx
@@ -5,19 +5,28 @@ const PlayerListItem = ({ player }) => (
         <h2>{player.playerTag}</h2>
         <h3>{player.totalScore}</h3>
         { player.health > 100 ?
-        <div className="progressParent">
+        <div className="progressParent"
+                style={{
+                    width: "200px",
+                    backgroundColor: `grey`, height: `20px`, color: "white", display: "flex", justifyContent: "center", verticalAlign: "center",
+                    borderRadius: "2px"}}>
                 <div style={{ width: `${player.health}%`, 
             backgroundColor: `#3500fa`, height: `100%`, color: "white", 
             fontSize: "14px", display: "flex", justifyContent: "center", verticalAlign: "center",
-                    borderRadius: "2px", paddingTop: "4px"
+                    borderRadius: "2px", paddingTop: "7px"
                 }}><i className="fas fa-shield-alt">&#160;&#160;{player.health}%</i></div>
         </div>
 			    :
-        <div className="progressParent">
+        <div className="progressParent"
+                style={{
+                    width: "200px",
+                    backgroundColor: `grey`, height: `20px`, color: "white", display: "flex", justifyContent: "center", verticalAlign: "center",
+                    borderRadius: "2px"
+                }}>
                 <div style={{ width: `${player.health}%`, backgroundColor: `red`, 
                     height: `100%`, color: "white", fontSize: "14px", display: "flex", 
-                    justifyContent: "center", verticalAlign: "center", borderRadius: "2px", paddingTop: "4px"
-                }}><i class="fas fa-plus">&#160;&#160;{player.health}%</i></div>
+                    justifyContent: "center", verticalAlign: "center", borderRadius: "2px", paddingTop: "7px"
+                }}><i className="fas fa-plus">&#160;&#160;{player.health}%</i></div>
 		</div>
 				}
 	</li>
