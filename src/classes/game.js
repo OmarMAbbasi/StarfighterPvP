93259108f73692ca82574e69c5cd42b464c6f66f
Omar Abbasi <omarmabbasi@gmail.com>
2019-08-09 15:04:16 -0700

    Set round timer for production

--- src/classes/game.js
+++ src/classes/game.js
@@ -10,8 +10,8 @@ const FPS = 60;
 const sleep = ms => new Promise(res => setTimeout(res, ms));
 
 let hazardCount = 6;
-const NUM_ROUNDS = 1;
-const ROUND_LENGTH = 5;
+const NUM_ROUNDS = 5;
+const ROUND_LENGTH = 60;
 const START_LOCS = [
 	{ pos: new Vector2(200, 150), dir: new Vector2(1, 0) },
 	{ pos: new Vector2(1150, 600), dir: new Vector2(-1, 0) },
@@ -114,7 +114,7 @@ class Game {
 		Object.values(this.players).forEach(player => player.setDefaults());
 
 		// assign random power up
-		for (let i=0; i < pickOrder.length; i++) {
+		for (let i = 0; i < pickOrder.length; i++) {
 			const player = pickOrder[i];
 			const choice = Math.floor(Math.random() * powerUps.length);
 			player.applyPowerUp(powerUps[choice]);
